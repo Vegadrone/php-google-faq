6ca7e0c9a4c5779facdad4f5ad015148e649e906
Vegadrone <spenaantonino@gmail.com>
2022-08-24 15:20:51 +0200

    basic html structure

---
+++ index.php
@@ -0,0 +1,63 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- MY STYLE -->
+    <link rel="stylesheet" href="./styles/style.css">
+    <!-- BOOTSTRAP IMPORT v5.2 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+
+    <title>Domande frequenti - Privacy e termini - Google</title>
+</head>
+
+<body>
+
+    <header>
+        <h2>Privacy e termini</h2>
+        <nav>
+            <!-- Create a data structure and use it to populates the menu dynamically -->
+            <ul>
+                <li>Introduzione</li>
+                <li>Norme sulla privacy</li>
+                <li>Termini di servizio</li>
+                <li>Tecnologie</li>
+                <li>Domande frequenti</li>
+            </ul>
+        </nav>
+
+        <hr>
+
+    </header>
+
+    <main>
+        <h1>Domanda</h1>
+        <p>testo della domanda</p>
+    </main>
+
+    <footer>
+        <ul>
+            <li>Google</li>
+            <li>Tutto su Google</li>
+            <li>Privacy</li>
+            <li>Termini</li>
+        </ul>
+        <select name="language" id="">
+            <option value="Italian">Italiano</option>
+            <option value="Italian">Italiano</option>
+            <option value="Italian">Italiano</option>
+            <option value="Italian">Italiano</option>
+            <option value="Italian">Italiano</option>
+            <option value="Italian">Italiano</option>
+            <option value="Italian">Italiano</option>
+            <option value="Italian">Italiano</option>
+            <option value="Italian">Italiano</option>
+            <option value="Italian">Italiano</option>
+            <option value="Italian">Italiano</option>
+        </select>
+    </footer>
+</body>
+
+</html>
